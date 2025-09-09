9b6a22566f4e80fce3d6c6d8be243985df5896d2
jayakrishna-miriyam <jayamiriyam123@gmail.com>
2025-09-09 19:14:25 -0400

    Updated SEO, Sitemap.xml, robots.txt files, jpeg -> webP

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,8 +11,8 @@ export const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
         <header className="sticky top-0 z-50 w-full border-b border-slate-200 dark:border-slate-800 bg-white/80 dark:bg-slate-900/80 backdrop-blur-sm">
             <div className="container mx-auto px-4 h-16 flex items-center justify-between">
                 <a href="/" aria-label="AgeCalculater.com Homepage">
-                    <img src="https://res.cloudinary.com/miryam/image/upload/v1757150054/agecalculater.com/assets/age-calculater-logo-black.png" alt="AgeCalculater.com Logo" className="h-12 block dark:hidden" />
-                    <img src="https://res.cloudinary.com/miryam/image/upload/v1757150054/agecalculater.com/assets/age-calculater-logo-white.png" alt="AgeCalculater.com Logo" className="h-12 hidden dark:block" />
+                    <img src="https://res.cloudinary.com/miryam/image/upload/v1757368189/agecalculater.com/assets/age-calculater-logo-black_webP.webp" alt="AgeCalculater.com Logo" className="h-12 block dark:hidden" />
+                    <img src="https://res.cloudinary.com/miryam/image/upload/v1757368190/agecalculater.com/assets/age-calculater-logo-white_webP.webp" alt="AgeCalculater.com Logo" className="h-12 hidden dark:block" />
                 </a>
                 <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
             </div>
